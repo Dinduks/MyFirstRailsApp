130eb539e8bbafbd63cae607c776a616d123530e
Samy Dindane <samy@dindane.com>
2011-12-14 11:49:41 +0100

    Ajout de données d'exemple

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create([
+    {
+      email:       'samy@dindane.com', 
+      password:    'samysawesome',
+      admin:       true
+    },
+    {
+      email:       'hello@world.com', 
+      password:    'helloworld'
+    },
+ ])
+
+Article.create([
+    {
+      titre:       'KORG X50',
+      image:       '8714701_1.jpg',
+    },
+    {
+      titre:       'An arrow to the knee ',
+      image:       'arrow.JPG',
+      description: 'If you used to be an adventurer like me...'
+    },
+    {
+      titre:       'SSD',
+      image:       'ssd-test-jeu-video.jpg',
+    },
+])
