4ffa2b04ce6ac245f57382cb4147a79de045a880
Samy Dindane <samy@dindane.com>
2011-12-15 10:10:36 +0100

    Added the get_cart_status helper

--- app/helpers/panier_helper.rb
+++ app/helpers/panier_helper.rb
@@ -1,2 +1,17 @@
 module PanierHelper
+  def get_cart_status
+    if !session[:panier].nil? && !session[:panier].empty?
+      cart_counter = session[:panier].count.to_s
+      if session[:panier].count == 1
+        cart_counter += " "
+        cart_counter += I18n.t :produit
+      else
+        cart_counter += " "
+        cart_counter += I18n.t :produits
+      end
+    else
+      cart_counter = I18n.t :vide
+    end
+    return cart_counter
+  end
 end
