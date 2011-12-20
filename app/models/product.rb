c6f0d2ddaa76db853c6505979f410488f05ee2c2
Samy Dindane <samy@dindane.com>
2011-12-20 21:55:56 +0100

    Overrode the price getter for Product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ActiveRecord::Base
+
+  def price
+    price = self[:price]
+    if price % price.to_i == 0
+      return price.to_i
+    else
+      return price
+    end
+  end
+
 end
