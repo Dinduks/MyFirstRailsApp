065a0db729c37c8c8c67c615af92be1e3c4e0dc2
Samy Dindane <samy@dindane.com>
2011-12-23 17:41:03 +0100

    Reorganized views files

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Ecommerce::Application.routes.draw do
       match "/recommander" => "products#recommander",  :as => "recommander"
     end
     match "/forbidden"     => "application#forbidden", :as => "forbidden"
-    match "/:locale/"      => "index#index", :as => "index"
-    match "/"      => "index#index", :as => "index"
+    match "/:locale/"      => "index#index",           :as => "index"
+    match "/"              => "index#index",           :as => "index"
     root :to => "index#index"
   end
 end
